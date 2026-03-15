a7a34a6d4e95eb2a33ffd27c6102f2ce0b1e4794
Emmanuel Nguessan <mmguessan678@gmail.com>
2026-03-15 21:49:22 +0000

    unencrypted version

--- manager.py
+++ manager.py
@@ -1,1 +1,31 @@
-pwd = input("what is the master password?")
+pwd = input("what is the master password? ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
+
+
+
+def add():
+    name = input('Account Name : ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add)? . Press q to quit     ").lower()
+
+    if mode == 'q':
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
